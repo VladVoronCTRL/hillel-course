f20ec0da6e842f4d07e548b5c72927238134f669
VladVoronCTRL <yamahayasha@gmail.com>
2024-08-08 18:56:38 +0300

    fourth commit

--- fourth_project.py
+++ fourth_project.py
@@ -1,18 +1,3 @@
-number = input("введіть перше число: ")
-number = int(number)
-action = input("введіть дію")
-number_2 = input("введіть друге чилсо")
-number_2 = int(number_2)
-if action == "+":
-    print(number + number_2)
-elif action == "-":
-    print(number - number_2)
-elif action == "*":
-    print(number * number_2)
-elif action == "/":
-    if number_2 == 0:
-        print("на нуль ділити не можна ")
-
-    else:
-        action = int(number / number_2)
-        print(action)
+lst = [1, 2, 3, 4]
+lst_2 = [lst[-1]] + lst[:-1]
+print(lst_2)
