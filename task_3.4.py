5fe04cb1bc6dddfb8dcd0e54a2e2f1acdd3924c1
VladVoronCTRL <yamahayasha@gmail.com>
2024-08-10 10:58:52 +0300

    fifth commit

--- task_3.4.py
+++ task_3.4.py
@@ -3,7 +3,7 @@ if not lst:
     result = 0
 else:
     even_index_sum = 0
-for index,value in enumerate(lst):
+for index, value in enumerate(lst):
     if index % 2 == 0:
         even_index_sum += value
         result = even_index_sum * lst[-1]
